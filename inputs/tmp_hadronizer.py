9ab00b9db7ee929060dd415d4b98adb8df760769
SiewYan <siew.yan.hoh@cern.ch>
2019-04-12 13:45:22 +0200

    template hadronizer, Hadronizer_TuneCUETP8M1_13TeV_generic_LHE_pythia8_cff.py

--- inputs/tmp_hadronizer.py
+++ inputs/tmp_hadronizer.py
@@ -1,28 +1,20 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 
-from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
 generator = cms.EDFilter("Pythia8HadronizerFilter",
-                         ExternalDecays = cms.PSet(
-    Tauola = cms.untracked.PSet(
-    TauolaPolar,
-    TauolaDefaultInputCards
-    ),
-    parameterSets = cms.vstring('Tauola')
-    ),
-                         UseExternalGenerators = cms.untracked.bool(True),
-                         
                          maxEventsToPrint = cms.untracked.int32(1),
                          pythiaPylistVerbosity = cms.untracked.int32(1),
                          filterEfficiency = cms.untracked.double(1.0),
                          pythiaHepMCVerbosity = cms.untracked.bool(False),
                          comEnergy = cms.double(13000.),
                          PythiaParameters = cms.PSet(
-    processParameters = cms.vstring(
-    'Main:timesAllowErrors    = 10000', 
-    'ParticleDecays:limitTau0 = on',
-    'ParticleDecays:tauMax = 10',
-    'Tune:ee 3',
-    'Tune:pp 5'),
-    parameterSets = cms.vstring('processParameters')
-    )
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings'
+                                    )
+        )
                          )
+
+ProductionFilterSequence = cms.Sequence(generator)
